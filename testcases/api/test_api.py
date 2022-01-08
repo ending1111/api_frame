dda4fa50f3408826953c1b2c14684cd618a3fafb
zhouli <zhouli@wisonic.com>
2022-01-08 15:53:05 +0800

    Initial commit

---
+++ testcases/api/test_api.py
@@ -0,0 +1,12 @@
+#小莉莉
+import time
+
+
+class TestApi:
+
+    def test_01_mashang(self):
+        print("我是第一个编辑的用例")
+    def test_02_教育(self):
+        print("我是第二个编辑的用例")
+    def test_03_百里(self):
+        print("我是第三个编辑的用例")
